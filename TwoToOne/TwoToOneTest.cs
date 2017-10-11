981ebf21cd20ad481cc7abb57131aaa6554f0272
cs4171 <cs4171@SKCS4171>
2017-10-11 13:50:26 +0800

    Add a failed test case : when input ("a", "a"), should be return "a".

---
+++ TwoToOne/TwoToOneTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoToOne
+{
+    [TestFixture]
+    public class TwoToOneTest
+    {
+        [Test]
+        public void Test_When_input_s1_is_a_and_s2_is_a_should_be_a()
+        {
+            Assert.AreEqual("a", TwoToOneSolution.Longest("a", "a"));
+        }
+    }
+
+    public static class TwoToOneSolution
+    {
+        public static string Longest(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
