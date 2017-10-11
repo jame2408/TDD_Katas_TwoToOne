5011bff36585da4434f5d7bbe658ddd887c83800
cs4171 <cs4171@SKCS4171>
2017-10-11 14:06:28 +0800

    Pass test case : when input ("b", "a"), should be return "ab".

--- TwoToOne/TwoToOneTest.cs
+++ TwoToOne/TwoToOneTest.cs
@@ -33,7 +33,9 @@ namespace TwoToOne
     {
         public static string Longest(string s1, string s2)
         {
-            return s1 == s2 ? s1 : (s1 + s2);
+            var result = (s1 + s2).ToArray();
+            Array.Sort(result);
+            return String.Join("", result.Distinct());
         }
     }
 }
